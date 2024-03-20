9f2fda285078cd518464387a805ddf9cc736a783
PeiYingWu100 <peiying.wu100@gmail.com>
2024-03-20 15:05:36 +1100

    Add function: loadScript so it can be called with the location later

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,34 +2,39 @@ import { useEffect, useState } from "react";
 import { AxiosError, AxiosRequestConfig, CanceledError } from "axios";
 import apiClient from "../services/apiClient";
 
+
 const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig) =>{
-    const [data, setData] = useState<T | null>(null);
-    const [error, setError] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
-  
-    useEffect(() => {
-        const controller = new AbortController();
+    const loadScript = () => {
+        const [data, setData] = useState<T | null>(null);
+        const [error, setError] = useState("");
+        const [isLoading, setIsLoading] = useState(false);
+    
+        useEffect(() => {
+            const controller = new AbortController();
 
-        setIsLoading(true);
-        apiClient
-            .get<T>(endpoint, { signal: controller.signal, ...requestConfig })
-            .then((res) => {
-                setData(res.data);
-                if(data) setError("");
-                setIsLoading(false)
-            })
-            .catch((err: Error | AxiosError) => {
-                if(err instanceof CanceledError) return;
+            setIsLoading(true);
+            apiClient
+                .get<T>(endpoint, { signal: controller.signal, ...requestConfig })
+                .then((res) => {
+                    setData(res.data);
+                    if(data) setError("");
+                    setIsLoading(false)
+                })
+                .catch((err: Error | AxiosError) => {
+                    if(err instanceof CanceledError) return;
 
-                setError(err.message);
-                setIsLoading(false)
-            })
-            // .finally(() => setIsLoading(false));
+                    setError(err.message);
+                    setIsLoading(false)
+                })
+                // .finally(() => setIsLoading(false));
 
-            return () => controller.abort();
-    }, []);
+                return () => controller.abort();
+        }, []);
 
-    return {data, error, isLoading}
+        return {data, error, isLoading}
+    }
+
+    return { loadScript }
 }
 
 export default useData;
