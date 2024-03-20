4fe35f4f103308459ebf15c984a903604ba76a56
PeiYingWu100 <peiying.wu100@gmail.com>
2024-03-20 19:45:27 +1100

    Merge three useState into one (data, error, isLoading => weatherCardQuery)

--- src/hooks/useCurrentLocation.ts
+++ src/hooks/useCurrentLocation.ts
@@ -3,11 +3,12 @@ import { useEffect, useState } from "react";
 import { WeatherData } from "./useWeather";
 import apiClient from "../services/apiClient";
 import { LocationQuery } from "../components/SearchBar";
+import { WeatherCardQuery } from "../App";
 
 const useCurrentLocation = () => {
-    const [data, setData] = useState<WeatherData | null>(null);
-    const [error, setError] = useState("");
-    const [isLoading, setIsLoading] = useState(true);
+    const [weatherCardQuery, setWeatherCardQuery] = useState<WeatherCardQuery>(
+        {} as WeatherCardQuery
+    );
 
     function fetchWeather(locationQuery: LocationQuery ){
         apiClient
@@ -17,19 +18,45 @@ const useCurrentLocation = () => {
                 `/data/2.5/weather?lat=${locationQuery.lat}&lon=${locationQuery.lon}&units=metric`
             )
             .then((res) => {
-                setData(res.data);
-                if(data) setError("");
-
-                setIsLoading(false)
+                setWeatherCardQuery({
+                    data: res.data,
+                    error: res.data ? "" : "empty data",
+                    isLoading: false,
+                });
             })
             .catch((err: Error | AxiosError) => {
-                setError(err.message);
-
-                setIsLoading(false)
+                setWeatherCardQuery({
+                    data: null,
+                    error: err.message,
+                    isLoading: false,
+                });
             });
     }
 
     useEffect(() => {        
+        setWeatherCardQuery({
+            data: null,
+            error: "",
+            isLoading: true,
+          });
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success,failed);
+        }
+        else {
+            setWeatherCardQuery({
+                data: null,
+                error: "Unfortunately, your browser doesn't support geolocation. As a fallback, we've set the default location to Sydney.",
+                isLoading: false,
+            });
+
+            fetchWeather({
+                location: "Sydney",
+                lat: null,
+                lon: null,
+            })
+        }
+
         function success(position: GeolocationPosition){
             const { latitude, longitude } = position.coords;
 
@@ -41,21 +68,12 @@ const useCurrentLocation = () => {
         }
 
         function failed(locationErr:GeolocationPositionError){
-            setError(`Apologies, we encountered an error while retrieving your location: ${locationErr.message}. As a fallback, we've set the default location to Sydney.`);
-
-            fetchWeather({
-                location: "Sydney",
-                lat: null,
-                lon: null,
-            })
-        }
-
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(success,failed);
-        }
-        else {
-            setError("Unfortunately, your browser doesn't support geolocation. As a fallback, we've set the default location to Sydney.");
-
+            setWeatherCardQuery({
+                data: null,
+                error: `Apologies, we encountered an error while retrieving your location: ${locationErr.message}. As a fallback, we've set the default location to Sydney.`,
+                isLoading: false,
+            });
+           
             fetchWeather({
                 location: "Sydney",
                 lat: null,
@@ -64,7 +82,7 @@ const useCurrentLocation = () => {
         }
     }, []);
 
-    return {data, error, isLoading}
+    return {...weatherCardQuery}
 }
 
 export default useCurrentLocation;
